87e3dfb8f3d27b040628e259459a6017e7450de9
StamGio <stamatisgio@icloud.com>
2023-06-04 16:28:11 +0300

    Add Grid Template with Chakra Ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
-import { Box, ChakraProvider, extendTheme } from "@chakra-ui/react";
-
-const colors = {
-  brand: {
-    50: "#FFFAF0",
-    100: "#FEEBC8",
-    200: "#FBD38D",
-    300: "#F6AD55",
-    400: "#ED8936",
-    500: "#DD6B20",
-    600: "#C05621",
-    700: "#9C4221",
-    800: "#7B341E",
-    900: "#652B19",
-  },
-};
-
-const theme = extendTheme({ colors });
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 const App = () => {
   return (
-    <ChakraProvider theme={theme}>
-      <Box bg="brand.600" w="100vw" h="100vh">
-        <div>MARIOS ALVANOS</div>
-      </Box>
-    </ChakraProvider>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main" "footer"`,
+        lg: `"nav nav"
+                    "aside main"
+                    "aside footer"`,
+      }}
+      color="blackAlpha.700"
+    >
+      <GridItem pl="2" bg="orange.300" area={"nav"}>
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem pl="2" bg="pink.300" area={"aside"}>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem pl="2" bg="green.300" area={"main"}>
+        Main
+      </GridItem>
+      <GridItem pl="2" bg="blue.300" area={"footer"}>
+        Footer
+      </GridItem>
+    </Grid>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,34 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 // Chakra UI imports
-import { ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 // Extend the theme to include custom colors, fonts, etc
+
+const colors = {
+  brand: {
+    50: "#FFFAF0",
+    100: "#FEEBC8",
+    200: "#FBD38D",
+    300: "#F6AD55",
+    400: "#ED8936",
+    500: "#DD6B20",
+    600: "#C05621",
+    700: "#9C4221",
+    800: "#7B341E",
+    900: "#652B19",
+  },
+};
+
+const theme = extendTheme({ colors });
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
+    <ChakraProvider theme={theme}>
+      <Box bg="brand.600" w="100vw" h="100vh">
+        <App />
+      </Box>
     </ChakraProvider>
   </React.StrictMode>,
   rootElement
