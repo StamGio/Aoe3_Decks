26bbdbed8f0193d9b8b78bb274594eb8cf84c321
StamGio <stamatisgio@icloud.com>
2023-06-07 20:12:24 +0300

    Write DLCs and Civs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
 // Chakra Ui imports
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Grid, GridItem, Show, Text } from "@chakra-ui/react";
 
 // Components Imports
 import Navbar from "./Components/Navbar";
-import CountryList from "./Components/CivilizationsList";
+import CivilizationsList from "./Components/CivilizationsList";
 
-const civilizations = ["civ 1", "civ 2", "civ 3"];
+//////////////////////  Civilitations Array /////////////////////////
 
+// Vanilla
+const vanillaCivs = [
+  "British",
+  "Dutch",
+  "French",
+  "Germans",
+  "Ottoman",
+  "Portuguese",
+  "Russians",
+  "Spanish",
+];
+
+// The War Chiefs
+const warChiefsCivs = ["Aztecs", "Haudenosaunee", "Lacota"];
+
+// The Asian Dynastys
+
+const asianDynastiesCivs = ["Chinese", "Indians", "Japanese"];
+
+// Definitive Edition
+const definitiveCivs = ["Incas", "Swedesh"];
+
+// Definitive Edition II
+const definitive2Civs = ["Mexico", "United States"];
+
+//The  African Royaltes
+const africanRoyaltesCivs = ["Ethiopians", "Hausa"];
+
+// Knights of the Mediterranean
+const knightsOfMedCivs = ["Italians", "Maltese"];
+////////////////////////////// End ///////////////////////////////////
 const App: React.FC = () => {
   return (
     <Grid
@@ -22,9 +53,19 @@ const App: React.FC = () => {
         <Navbar />
       </GridItem>
       <Show above="lg">
-        <GridItem pl="2" bg="pink.300" area={"aside"}>
-          <h1>List of Civilizations</h1>
-          <CountryList civilizations={civilizations} />
+        <GridItem pl="2" area={"aside"}>
+          <Text as="h2" className="styled-h2">
+            Civilizations
+          </Text>
+          <CivilizationsList
+            vanillaCivs={vanillaCivs}
+            warChiefsCivs={warChiefsCivs}
+            asianDynastiesCivs={asianDynastiesCivs}
+            definitiveCivs={definitiveCivs}
+            definitive2Civs={definitive2Civs}
+            africanRoyaltesCivs={africanRoyaltesCivs}
+            knightsOfMedCivs={knightsOfMedCivs}
+          />
         </GridItem>
       </Show>
       <GridItem pl="2" bg="green.300" area={"main"}>
--- src/Components/CivilizationsList.tsx
+++ src/Components/CivilizationsList.tsx
@@ -1,18 +1,94 @@
+import { List, ListItem, VStack, Text, Avatar } from "@chakra-ui/react";
 import React from "react";
 
-type CivilizationsListProps = {
-  civilizations: string[];
-};
+interface CivilizationsListProps {
+  vanillaCivs: string[];
+  warChiefsCivs: string[];
+  asianDynastiesCivs: string[];
+  definitiveCivs: string[];
+  definitive2Civs: string[];
+  africanRoyaltesCivs: string[];
+  knightsOfMedCivs: string[];
+}
 
 const CivilizationsList: React.FC<CivilizationsListProps> = ({
-  civilizations,
+  vanillaCivs,
+  warChiefsCivs,
+  asianDynastiesCivs,
+  definitiveCivs,
+  definitive2Civs,
+  africanRoyaltesCivs,
+  knightsOfMedCivs,
 }) => {
   return (
-    <ul>
-      {civilizations.map((civilization, index) => (
-        <li key={index}>{civilization}</li>
-      ))}
-    </ul>
+    <>
+      <VStack align="flex-start">
+        <Text as="h4" className="styled-h4">
+          Vanilla Civilizations
+        </Text>
+        <List color={"white"} fontFamily={"TrajanPro"} colorScheme="#ebc837">
+          {vanillaCivs.map((vanillaCiv, index) => (
+            <ListItem key={index} display="flex" alignItems="center">
+              {/* <Avatar
+                size="sm"
+                src="../src/assets/Images/Flags/english.png"
+                borderRadius="md"
+                mr={2}
+              /> */}
+              {vanillaCiv}
+            </ListItem>
+          ))}
+        </List>
+        <Text as="h4" className="styled-h4">
+          The War Chiefs
+        </Text>
+        <List color={"white"} fontFamily={"TrajanPro"} colorScheme="#ebc837">
+          {warChiefsCivs.map((warChiefsCiv, index) => (
+            <ListItem key={index}>{warChiefsCiv}</ListItem>
+          ))}
+        </List>
+        <Text as="h4" className="styled-h4">
+          The Asian Dynasties
+        </Text>
+        <List color={"white"} fontFamily={"TrajanPro"} colorScheme="#ebc837">
+          {asianDynastiesCivs.map((asianDynastiesCiv, index) => (
+            <ListItem key={index}>{asianDynastiesCiv}</ListItem>
+          ))}
+        </List>
+        <Text as="h4" className="styled-h4">
+          Definitive Edition
+        </Text>
+        <List color={"white"} fontFamily={"TrajanPro"} colorScheme="#ebc837">
+          {definitiveCivs.map((definitiveCiv, index) => (
+            <ListItem key={index}>{definitiveCiv}</ListItem>
+          ))}
+        </List>
+        <Text as="h4" className="styled-h4">
+          Definitive Edition II
+        </Text>
+        <List color={"white"} fontFamily={"TrajanPro"} colorScheme="#ebc837">
+          {definitive2Civs.map((definitive2Civ, index) => (
+            <ListItem key={index}>{definitive2Civ}</ListItem>
+          ))}
+        </List>
+        <Text as="h4" className="styled-h4">
+          The African Royaltes
+        </Text>
+        <List color={"white"} fontFamily={"TrajanPro"} colorScheme="#ebc837">
+          {africanRoyaltesCivs.map((africanRoyaltesCiv, index) => (
+            <ListItem key={index}>{africanRoyaltesCiv}</ListItem>
+          ))}
+        </List>
+        <Text as="h4" className="styled-h4">
+          Knights of the Mediterranean
+        </Text>
+        <List color={"white"} fontFamily={"TrajanPro"} colorScheme="#ebc837">
+          {knightsOfMedCivs.map((knightsOfMedCiv, index) => (
+            <ListItem key={index}>{knightsOfMedCiv}</ListItem>
+          ))}
+        </List>
+      </VStack>
+    </>
   );
 };
 
