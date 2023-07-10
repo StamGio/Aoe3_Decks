89e6e425331aa6be2cf66f2560d2a232322e1b4d
StamGio <stamatisgio@icloud.com>
2023-07-11 02:29:52 +0300

    photos for decks , back icon

--- src/Components/Decks.tsx
+++ src/Components/Decks.tsx
@@ -1,8 +1,8 @@
-import { Box, Heading, Image, Text } from "@chakra-ui/react";
+import { Box, Heading, Image, Text, VStack } from "@chakra-ui/react";
 import React from "react";
 import woodImage from "../assets/Images/wood.png";
 
-type Civilization = "British" | "Germans";
+type Civilization = "British" | "Germans" | "Dutch";
 
 interface CivPhoto {
   title: string;
@@ -12,18 +12,33 @@ interface CivPhoto {
 interface CivPhotos {
   [key: string]: CivPhoto[];
 }
+// Objects List
 
 const civPhotos: CivPhotos = {
   British: [
-    { title: "Treaty no grenadiers", deck: "TreatyNoGrenadiers.png" },
-    { title: "Treaty grenadiers", deck: "aoe3_logo.png" },
-    { title: "Treaty sheeps", deck: "aoe3_logo.png" },
-    { title: "1 vs 1", deck: "aoe3_logo.png" },
-    { title: "Treaty stonemasons", deck: "aoe3_logo.png" },
-    { title: "1 vs 1 grenadier (outdated)", deck: "aoe3_logo.png" },
-    { title: "Late game water", deck: "aoe3_logo.png" },
+    { title: "Treaty No Grenadiers", deck: "TreatyNoGrenadiers.png" },
+    { title: "Treaty Grenadiers", deck: "TreatyGrenadiers.png" },
+    { title: "Treaty Sheeps", deck: "TreatySheeps.png" },
+    { title: "Treaty Stonemasons", deck: "Treatystonemasons.png" },
+    { title: "1 vs 1", deck: "1vs1.png" },
+    { title: "1 vs 1 Grenadier (Outdated)", deck: "1vs1Grenadier.png" },
+    { title: "Late Game Water", deck: "LateGameWater.png" },
   ],
-  Germans: [{ title: "Main", deck: "germans_logo.png" }],
+  Germans: [
+    { title: "1 vs 1 Standard", deck: "1vs1std.png" },
+    { title: "Germany Late Game", deck: "germanylategame.png" },
+    { title: "Mercenaries Late Game", deck: "germanyMercenarieslategame.png" },
+  ],
+  Dutch: [
+    { title: "1 vs 1 Standard", deck: "1vs1std.png" },
+    { title: "1 vs 1 Modern", deck: "1vs1modern.png" },
+    { title: "1 vs 1 Old School", deck: "1vs1oldschool.png" },
+    {
+      title: "1 vs 1 Treaty Mercenaries Only",
+      deck: "treatymercenariesonly.png",
+    },
+    { title: "1 vs 1 Late Game Natives", deck: "lategamenatives.png" },
+  ],
 };
 
 interface DecksProps {
@@ -64,32 +79,39 @@ const Decks: React.FC<DecksProps> = ({ civilization }) => {
           </Text>
         </Box>
       </Heading>
-      <Box
-        textAlign="left"
-        maxWidth="980px"
-        padding="20px"
-        bg={`url(${woodImage})`}
-        borderBottom="1px solid rgb(235, 200, 55)"
-        borderTop="1px solid rgb(235, 200, 55)"
-        className="bordercolor"
-      >
-        {civilizationPhotos &&
-          civilizationPhotos.map((photo, index) => (
-            <Box
-              key={index}
-              display="flex"
-              flexDirection="column"
-              alignItems="center"
-              mb="10px"
-            >
-              <span>{photo.title}</span>{" "}
-              <Image
-                src={`../src/assets/Images/Decks/${photo.deck}`}
-                alt={`Photo ${index + 1}`}
-              />
-            </Box>
-          ))}
-      </Box>
+
+      {/* Decks Photos */}
+
+      <VStack>
+        <Box
+          textAlign="left"
+          maxWidth="980px"
+          padding="20px"
+          bg={`url(${woodImage})`}
+          borderBottom="1px solid rgb(235, 200, 55)"
+          borderTop="1px solid rgb(235, 200, 55)"
+          className="bordercolor"
+        >
+          {civilizationPhotos &&
+            civilizationPhotos.map((photo, index) => (
+              <Box
+                key={index}
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                mb="10px"
+                padding={2}
+              >
+                <Text className="styled-h4">{photo.title}</Text>{" "}
+                <Image
+                  src={`../src/assets/Images/Decks/${civilization}/${photo.deck}`}
+                  alt={`Photo ${index + 1}`}
+                  mb="3rem"
+                />
+              </Box>
+            ))}
+        </Box>
+      </VStack>
     </Box>
   );
 };
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,13 +1,29 @@
-import { Box, Flex, Img, Link } from "@chakra-ui/react";
+import { Box, Button, Flex, Img, Link } from "@chakra-ui/react";
+import { BsBack } from "react-icons/bs";
 
 const Navbar = () => {
+  const handleGoBack = () => {
+    window.history.back();
+  };
+
   return (
     <Flex
       className="navbar navbar-expand-lg navbar-light bordercolor"
-      bgGradient="linear-gradient(90deg, rgb(23, 8, 3), rgb(83, 36, 18) 40%, rgb(23, 8, 3))"
+      bgGradient="linear-gradient(90deg, rgb(23, 8, 3), #532412 40%, rgb(23, 8, 3))"
     >
-      <Box className="container-fluid d-flex justify-content-center align-items-center">
-        <Link href="http://127.0.0.1:5173">
+      <Button
+        className="justify-content-left align-items-left d-flex"
+        marginLeft={7}
+        color="#532412"
+        borderColor="#532412"
+        variant="outline"
+        onClick={handleGoBack}
+      >
+        <BsBack />
+      </Button>
+
+      <Box className="container-fluid justify-content-center align-items-center">
+        <Link href="http://127.0.0.1:5173" marginRight={8}>
           <Img
             src="../src/assets/Images/aoe3_logo.png"
             alt="AOE3 Logo"
